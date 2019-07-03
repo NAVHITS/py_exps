c2ddc1eeb54d4ea155088330b2fe6534771b7004
Naveen S <naveen2aug@gmail.com>
2019-07-03 11:24:54 +0530

    Update set5.py

--- beginner/set5.py
+++ beginner/set5.py
@@ -1,1 +1,10 @@
-
+def max(a, b, c):
+  if a > b and a > c:
+    big = a
+  elif b > a and b > c:
+    big = b
+  else:
+    big = c
+  return big
+a, b, c = map(int,input().split())
+print(max(a, b, c))
