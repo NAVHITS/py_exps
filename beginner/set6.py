287734748c82540b9024f5ec7ca178441f4a9e67
Naveen S <naveen2aug@gmail.com>
2019-07-03 11:58:32 +0530

    Update set6.py

--- beginner/set6.py
+++ beginner/set6.py
@@ -2,10 +2,10 @@ yr = int(input())
 if yr % 4 == 0:
   if yr % 100 == 0:
     if yr % 400 == 0:
-      print("Yes")
+      print("yes")
     else:
-      print("No")
+      print("no")
   else:
-    print("Yes")
+    print("yes")
 else:
-  print("No")
+  print("no")
