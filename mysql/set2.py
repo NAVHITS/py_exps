661d37ea25b3e78093a23afe5613f9b0e1e18996
Naveen S <naveen2aug@gmail.com>
2019-07-04 09:41:14 +0530

    Update set2.py

--- mysql/set2.py
+++ mysql/set2.py
@@ -1,1 +1,7 @@
 import mysql.connector
+db = mysql.connector.connect(host="localhost", port="3306",user="root",passwd="",database="test_db")
+query = db.cursor()
+data = "INSERT INTO test_tbl (name, dept) VALUES ("Naveen", "IT")"
+query.execute(data)
+db.commit()
+print(query.rowcount, "record inserted.")
