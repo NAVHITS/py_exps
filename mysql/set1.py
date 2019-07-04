f4f06d4057fdcc2b7cc72bc0d958160e35b9b00a
Naveen S <naveen2aug@gmail.com>
2019-07-04 09:39:15 +0530

    Update set1.py

--- mysql/set1.py
+++ mysql/set1.py
@@ -1,1 +1,5 @@
-
+import mysql.connector
+db = mysql.connector.connect(host="localhost", port="3306",user="root",passwd="",database="test_db")
+query = db.cursor()
+query.execute("CREATE TABLE test_tbl (name VARCHAR(25), dept VARCHAR(10))")
+db.commit()
