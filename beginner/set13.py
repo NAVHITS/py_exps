376e7bd7d6c23b4becea9d19b10f89d3943caf0d
Naveen S <naveen2aug@gmail.com>
2019-07-04 10:34:40 +0530

    Update set13.py

--- beginner/set13.py
+++ beginner/set13.py
@@ -1,12 +1,10 @@
 n = int(input())
-if n > 1:
-  for i in range(2,n):
-    if n % i == 0:
-      print("no")
-      break
-    else:
-      print("yes")
-      break
+flag = True
+for i in range(1, n):
+  if n % i == 0:
+    flag = False
+    break
+if flag == True:
+  print("yes")
 else:
   print("no")
-      
