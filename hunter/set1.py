4e7e1648fa500d7ff88f51191754bec681baaf7e
Naveen S <naveen2aug@gmail.com>
2019-07-05 12:41:12 +0530

    Update set1.py

--- hunter/set1.py
+++ hunter/set1.py
@@ -1,13 +1,13 @@
 from collections import Counter
 no = input()
-x = [i for i in input().split()]
-l = Counter(x)
-r = []
+lst = [i for i in input().split()]
+l = Counter(lst)
+res = []
 for i, j in l.items():
 	if j > 1:
-		r.append(i)
-if r:
-	r.sort()
-	print(' '.join(r))
+		res.append(i)
+if res:
+	res.sort()
+	print(' '.join(res))
 else:
 	print("unique")
