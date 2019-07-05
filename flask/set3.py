4474f47ab05a19aa23d7f02198cef3d177c31915
Naveen S <naveen2aug@gmail.com>
2019-07-05 21:38:38 +0530

    Update set3.py

--- flask/set3.py
+++ flask/set3.py
@@ -3,13 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def student():
-   return render_template('student.html')
+   return render_template('set3_1.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form
-      return render_template("result.html",result = result)
+      return render_template("set3_2.html",result = result)
 
 if __name__ == '__main__':
    app.run(debug = True)
