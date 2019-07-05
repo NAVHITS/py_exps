df8f5d4f59959dae6a16e32b7b48c26588ccc03a
Naveen S <naveen2aug@gmail.com>
2019-07-05 11:53:56 +0530

    Create set6.py

---
+++ player/set6.py
@@ -0,0 +1,13 @@
+def iso(x):
+  y = list(set([i for i in x]))
+  r = []
+  for i in y:
+      a.append(x.count(i))
+  return a
+val = input().split()
+t1 = iso(val[0])
+t2 = iso(val[1])
+if sorted(t1) == sorted(t2):
+  print("yes")
+else:
+  print("no")
