ae5c5d608a58b6c670f3a645252924c0789c954b
Naveen S <naveen2aug@gmail.com>
2019-07-07 18:39:41 +0530

    Update set2_s.py

--- networking/set2_s.py
+++ networking/set2_s.py
@@ -1,9 +1,10 @@
 import socket
 U_PORT = 5555
 UDP_IP = "127.0.0.1"
+BUF_SIZE = 1024
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 while True:
-    data, addr = sock.recvfrom(1024)
+    data, addr = sock.recvfrom(BUF_SIZE)
     print "Message: ", data
